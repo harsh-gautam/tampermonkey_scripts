9ce3a430cf5b9279923900ae9bcddbda332c7b2d
Harsh Gautam <hgautam208@gmail.com>
2024-02-11 14:18:13 +0530

    simplified add to watchlist function and styles

--- animepahe_anime_page/script.user.js
+++ animepahe_anime_page/script.user.js
@@ -15,20 +15,8 @@
     // Your code here...
     function addToWatchList(data, animeTitle, totalEpisodes) {
 
-        if(data === undefined || data === null) {
-            data = {};
-            data[animeTitle] = {"totalEpisodes": totalEpisodes, "episodesWatched": 0, "status": "Watching"};
-            alert("Added to watchlist!");
-        } else {
-            data = JSON.parse(data);
-            if (Object.keys(data).find(e => e === animeTitle)) {
-                alert("Already Watching!");
-            } else {
-                data[animeTitle] = {"totalEpisodes": totalEpisodes, "episodesWatched": 0, "status": "Watching"};
-                alert("Added to watchlist!");
-            }
-        }
-
+        data[animeTitle] = {"totalEpisodes": totalEpisodes, "episodesWatched": 0, "status": "Watching"};
+        alert("Added to watchlist!");
         localStorage.setItem("animepahe", JSON.stringify(data));
     }
 
@@ -42,11 +30,6 @@
 
     // Add the Add to watchlist button to DOM
     const japaneseTitle = document.querySelector('h2.japanese');
-    const addNode = document.createElement("button");
-    addNode.textContent = "+Add to watchlist";
-    addNode.style.backgroundColor = "red";
-    addNode.style.marginLeft = "10px";
-    addNode.style.padding = "5px";
 
     let data = localStorage.getItem("animepahe");
     if(data === null) {
@@ -56,40 +39,48 @@
         data = JSON.parse(data);
     }
 
-    let currentAnime;
     let watchedEpisode = 0;
     
     if(Object.keys(data).find(e => e === animeTitle)) {
-        currentAnime = data[animeTitle];
-        watchedEpisode = currentAnime.episodesWatched;
+
+        let currentAnime = data[animeTitle];
+
+        // Add watching status
+        const statusText = document.createElement("span")
+        statusText.textContent = currentAnime.status;
+        statusText.style = "background-color: #d5015b; padding: 5px; margin: auto 10px; border: 1px solid #d5015b; border-radius: 10px;";
+        japaneseTitle.appendChild(statusText);
+
+        // Create the input node
+        const episodeParent = document.createElement("span");
+        const episodeInput = document.createElement("input");
+        episodeInput.type = "text";
+        episodeInput.name = "episode-input";
+        episodeInput.value = currentAnime.episodesWatched;
+        episodeInput.style = "width: 5%; background-color: black; color: white; padding: 0; margin: auto 5px; text-align: center; border: 1px solid white; border-radius: 10px;";
+
+        
+        const episodeTotal = document.createElement("span");
+        episodeTotal.textContent = "/ " + episodeCount;
+
+
+        const episodeIncrease = document.createElement("i");
+        episodeIncrease.classList = "fa fa-plus-circle";
+        episodeIncrease.style = "margin: auto 5px;";
+
+        episodeParent.appendChild(episodeInput);
+        episodeParent.appendChild(episodeTotal);
+        episodeParent.appendChild(episodeIncrease);
+
+        japaneseTitle.appendChild(episodeParent);
+
+    } else {
+        const addButton = document.createElement("button");
+        addButton.textContent = "Add to list";
+        addButton.style = "background-color: #d5015b; color: white; padding: 5px; margin: auto 10px; border: 1px solid #d5015b; border-radius: 10px;"; 
+        addButton.addEventListener("click", function(){addToWatchList(data, animeTitle, episodeCount)});
+        japaneseTitle.appendChild(addButton);
     }
 
-    // Create the input node
-    const episodeParent = document.createElement("span");
-    const episodeInput = document.createElement("input");
-    episodeInput.type = "text";
-    episodeInput.name = "episode-input";
-    episodeInput.value = watchedEpisode;
-    episodeInput.style.backgroundColor = "black";
-    episodeInput.style.color = "white";
-    episodeInput.style.width = "5%";
-    episodeInput.style.padding = "0";
-    episodeInput.style.textAlign = "center";
-
-
-    const episodeTotal = document.createElement("span");
-    episodeTotal.textContent = "/" + episodeCount;
-
-    const episodeIncrease = document.createElement("i");
-    episodeIncrease.classList = ['fa-solid', 'fa-circel-plus'];
-
-    episodeParent.appendChild(episodeInput);
-    episodeParent.appendChild(episodeTotal);
-    episodeParent.appendChild(episodeIncrease);
-
-    addNode.addEventListener("click", function(){addToWatchList(data, animeTitle, episodeCount)});
-
-    japaneseTitle.appendChild(addNode);
-    japaneseTitle.appendChild(episodeParent);
     
 })();
