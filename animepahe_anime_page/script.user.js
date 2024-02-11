e23a7b862ad35d5db4ae2c504c97e9fd720163e5
Harsh Gautam <hgautam208@gmail.com>
2024-02-11 15:40:12 +0530

    display status after adding to watchlist

--- animepahe_anime_page/script.user.js
+++ animepahe_anime_page/script.user.js
@@ -13,51 +13,32 @@
     'use strict';
 
     // Your code here...
-    function addToWatchList(data, animeTitle, totalEpisodes) {
+    function addToWatchList(self, data, animeTitle, totalEpisodes, parentNode) {
 
         data[animeTitle] = {"totalEpisodes": totalEpisodes, "episodesWatched": 0, "status": "Watching"};
         alert("Added to watchlist!");
         localStorage.setItem("animepahe", JSON.stringify(data));
+
+        // remove the add to list button and display status
+        self.remove();
+        displayStatusButtons(data[animeTitle], parentNode);
     }
 
-    // Get the total episode count
-    const episodeCountElem = document.querySelector("div.anime-info > p:nth-child(4)").innerText;
-    const episodeCount = parseInt(episodeCountElem.slice(10));
-
-    // Get the title
-    const titleWrapper = document.querySelector("div.title-wrapper > h1 > span");
-    const animeTitle = titleWrapper.innerText;
-
-    // Add the Add to watchlist button to DOM
-    const japaneseTitle = document.querySelector('h2.japanese');
-
-    let data = localStorage.getItem("animepahe");
-    if(data === null) {
-        data = {};
-        localStorage.setItem("animepahe", JSON.stringify(data));
-    } else {
-        data = JSON.parse(data);
-    }
-
-    let watchedEpisode = 0;
-    
-    if(Object.keys(data).find(e => e === animeTitle)) {
-
-        let currentAnime = data[animeTitle];
-
+    function displayStatusButtons(currentAnime, parentNode) {
         // Add watching status
-        const statusText = document.createElement("span")
+        const statusText = document.createElement("div")
         statusText.textContent = currentAnime.status;
-        statusText.style = "background-color: #d5015b; padding: 5px; margin: auto 10px; border: 1px solid #d5015b; border-radius: 10px;";
-        japaneseTitle.appendChild(statusText);
+        statusText.style = "width: 250px; background-color: #d5015b; color: white; opacity: 0.9; padding: 5px; margin: 0 0 8px 240px; border: 1px solid #d5015b; border-radius: 10px;";
+        parentNode.appendChild(statusText);
 
         // Create the input node
         const episodeParent = document.createElement("span");
+        episodeParent.style = "padding: 5px;";
         const episodeInput = document.createElement("input");
         episodeInput.type = "text";
         episodeInput.name = "episode-input";
         episodeInput.value = currentAnime.episodesWatched;
-        episodeInput.style = "width: 5%; background-color: black; color: white; padding: 0; margin: auto 5px; text-align: center; border: 1px solid white; border-radius: 10px;";
+        episodeInput.style = "width: 20%; background-color: black; color: white; padding: 0; margin: auto 5px; text-align: center; border: 1px solid white; border-radius: 10px;";
 
         
         const episodeTotal = document.createElement("span");
@@ -72,14 +53,46 @@
         episodeParent.appendChild(episodeTotal);
         episodeParent.appendChild(episodeIncrease);
 
-        japaneseTitle.appendChild(episodeParent);
+        statusText.appendChild(episodeParent);
+
+    }
+
+    // Get the total episode count
+    const episodeCountElem = document.querySelector("div.anime-info > p:nth-child(4)").innerText;
+    const episodeCount = parseInt(episodeCountElem.slice(10));
+
+    // Get the title
+    const titleWrapper = document.querySelector("div.title-wrapper > h1 > span");
+    const animeTitle = titleWrapper.innerText;
+
+    // Add the Add to watchlist button to DOM
+    let parentNode = document.querySelector('div.title-wrapper');
+    // if (parentNode === null) {
+    //     parentNode = document.querySelector("div.title-wrapper > h1");
+    // } 
+
+    let data = localStorage.getItem("animepahe");
+    if(data === null) {
+        data = {};
+        localStorage.setItem("animepahe", JSON.stringify(data));
+    } else {
+        data = JSON.parse(data);
+    }
+
+    let watchedEpisode = 0;
+    
+    if(Object.keys(data).find(e => e === animeTitle)) {
+
+        let currentAnime = data[animeTitle];
+        displayStatusButtons(currentAnime, parentNode);
+        // displayRemoveButton(currentAnime, parentNode);
 
     } else {
         const addButton = document.createElement("button");
         addButton.textContent = "Add to list";
-        addButton.style = "background-color: #d5015b; color: white; padding: 5px; margin: auto 10px; border: 1px solid #d5015b; border-radius: 10px;"; 
-        addButton.addEventListener("click", function(){addToWatchList(data, animeTitle, episodeCount)});
-        japaneseTitle.appendChild(addButton);
+        addButton.style = "width: 250px; background-color: #d5015b; color: white; opacity: 0.9; padding: 5px; margin: 0 0 8px 240px; border: 1px solid #d5015b; border-radius: 10px;"; 
+        addButton.addEventListener("click", function(){addToWatchList(addButton, data, animeTitle, episodeCount, parentNode)});
+        parentNode.appendChild(addButton);
     }
 
     
