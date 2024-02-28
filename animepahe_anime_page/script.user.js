40c78678eaf2c280555dfa020fdd1c8d42b38662
Harsh Gautam <hgautam208@gmail.com>
2024-02-29 00:18:59 +0530

    Fix increase episode watched count

--- animepahe_anime_page/script.user.js
+++ animepahe_anime_page/script.user.js
@@ -21,17 +21,18 @@
 
         // remove the add to list button and display status
         self.remove();
-        displayStatusButtons(data[animeId], parentNode);
+        displayStatusButtons(animeId, data[animeId], parentNode);
     }
 
-    function increaseEpisodeCount(animeId, episodeInput) {
+    function increaseEpisodeCount(animeId) {
         data = JSON.parse(localStorage.getItem("animepahe"));
         data[animeId].episodesWatched = data[animeId].episodesWatched + 1;
+        let episodeInput = document.querySelector("#episode-input");
         episodeInput.value = data[animeId].episodesWatched;
         localStorage.setItem("animepahe", JSON.stringify(data));
     }
 
-    function displayStatusButtons(currentAnime, parentNode) {
+    function displayStatusButtons(animeId, currentAnime, parentNode) {
         // Add watching status
         const statusText = document.createElement("div");
         statusText.textContent = currentAnime.status;
@@ -42,12 +43,13 @@
         const episodeParent = document.createElement("span");
         episodeParent.style = "padding: 5px;";
         const episodeInput = document.createElement("input");
+        episodeInput.id = "episode-input";
         episodeInput.type = "text";
         episodeInput.name = "episode-input";
         episodeInput.value = currentAnime.episodesWatched;
         episodeInput.style = "width: 20%; background-color: black; color: white; padding: 0; margin: auto 5px; text-align: center; border: 1px solid white; border-radius: 10px;";
 
-        
+
         const episodeTotal = document.createElement("span");
         episodeTotal.textContent = "/ " + episodeCount;
 
@@ -55,7 +57,7 @@
         const episodeIncrease = document.createElement("i");
         episodeIncrease.classList = "fa fa-plus-circle";
         episodeIncrease.style = "margin: auto 5px; cursor: pointer;";
-        episodeIncrease.addEventListener("click", function(){ increaseEpisodeCount(animeId, currentAnime, episodeInput)})
+        episodeIncrease.addEventListener("click", function(){ increaseEpisodeCount(animeId, currentAnime)});
 
         episodeParent.appendChild(episodeInput);
         episodeParent.appendChild(episodeTotal);
@@ -66,7 +68,10 @@
     }
 
     // Get the total episode count
-    const episodeCountElem = document.querySelector("div.anime-info > p:nth-child(4)").innerText;
+    let episodeCountElem = document.querySelector("div.anime-info > p:nth-child(4)").innerText;
+    if(!episodeCountElem.startsWith("Episodes")) {
+        episodeCountElem = document.querySelector("div.anime-info > p:nth-child(3)").innerText;
+    }
     const episodeCount = parseInt(episodeCountElem.slice(10));
 
     // Get the title
@@ -89,20 +94,20 @@
     }
 
     let watchedEpisode = 0;
-    
+
     if(Object.keys(data).find(e => e === animeId)) {
 
         let currentAnime = data[animeId];
-        displayStatusButtons(currentAnime, parentNode);
+        displayStatusButtons(animeId, currentAnime, parentNode);
         // displayRemoveButton(currentAnime, parentNode);
 
     } else {
         const addButton = document.createElement("button");
         addButton.textContent = "Add to list";
-        addButton.style = "width: 250px; background-color: #d5015b; color: white; opacity: 0.9; padding: 5px; margin: 0 0 8px 240px; border: 1px solid #d5015b; border-radius: 10px;"; 
+        addButton.style = "width: 250px; background-color: #d5015b; color: white; opacity: 0.9; padding: 5px; margin: 0 0 8px 240px; border: 1px solid #d5015b; border-radius: 10px;";
         addButton.addEventListener("click", function(){addToWatchList(addButton, data, animeId, animeTitle, episodeCount, parentNode)});
         parentNode.appendChild(addButton);
     }
 
-    
+
 })();
