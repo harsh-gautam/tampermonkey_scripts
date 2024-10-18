f18e8d6f68561091a016d392cc070ffc96c75b64
Harsh Gautam <hgautam208@gmail.com>
2024-10-19 01:28:46 +0530

    add green box on added anime in list

--- animepahe_anime_page/display.user.js
+++ animepahe_anime_page/display.user.js
@@ -5,19 +5,33 @@
 // @description  Show list of anime in watchlist on main screen
 // @author       You
 // @match        https://animepahe.ru
+// @match        https://animepahe.ru/?page=*
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
+// @require      https://raw.githubusercontent.com/CoeJoder/waitForKeyElements.js/refs/heads/master/waitForKeyElements.js
 // @grant        none
+// @run-at       document-idle
 // ==/UserScript==
 
 (function() {
     'use strict';
 
-    let contentWrapper = document.querySelector(".content-wrapper");
+    const data = JSON.parse(localStorage.getItem("animepahe"));
+    const IDs = Object.values(data).map(e => e.id)
 
-    let trackerWrapper = document.createElement("div");
-    trackerWrapper.setAttribute('tracker-wrapper');
-    trackerWrapper.textContent = "Watch List"
 
-    contentWrapper.appendChild(trackerWrapper);
+    function addBox(){
+
+        // Show green color on watching animes
+        let animeList = document.querySelectorAll("div[data-id]");
+
+        animeList.forEach(node => {
+            let animeId = node.getAttribute('data-id');
+            if(IDs.includes(animeId)) {
+                node.style.border = "2px solid green";
+            }
+        })
+    }
+    
+    waitForKeyElements ("div[data-id]", addBox, true);
 
 })();
